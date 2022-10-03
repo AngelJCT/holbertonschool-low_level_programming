c438f9216d242cf18a41325c88e516c784d44879
AngelJCT <acarrion_pr@yahoo.com>
2022-10-03 15:42:01 -0700

    Low and upper alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase and uppercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar ('\n')
+		return (0);
+}
