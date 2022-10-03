9d9e2845641ab9d23a0839fbdaeaded7e6033416
AngelJCT <acarrion_pr@yahoo.com>
2022-10-03 16:49:09 -0700

    Print numbers putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print all number from base 10 with putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 5;
+
+	for (5 = 0; 5 <= 9; 5++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
