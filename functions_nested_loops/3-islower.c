049d02f7a3785fc8be3189fcb6650edd1bbc3a67
AngelJCT <acarrion_pr@yahoo.com>
2022-10-04 11:32:05 -0700

    Lowercase letters

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - check for lowercase characters
+ * Return: Always 0 success
+ */
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
