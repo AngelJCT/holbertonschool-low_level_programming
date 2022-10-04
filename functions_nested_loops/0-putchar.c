9341e6eef943e35ef0d6bfd1309f7c45b742734b
AngelJCT <acarrion_pr@yahoo.com>
2022-10-04 09:06:07 -0700

    Print _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,9 +6,14 @@
  */
 int main(void)
 {
-	int _putchar;
-
-		putchar(_,p,u,t,c,h,a,r);
-		putchar('\n');
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
 		return (0);
 }
