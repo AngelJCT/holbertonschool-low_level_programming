05dd3ca1939c5a937e6541692521930d95a5d009
AngelJCT <acarrion_pr@yahoo.com>
2022-10-05 07:48:29 -0700

    24 hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,19 +13,18 @@ void jack_bauer(void)
 	for (y = '0'; y <= '2'; y++)
 	{
 		_putchar(y);
-	}
-	for (z = '0'; z <= '3'; z++)
-	{
+		
+		for (z = '0'; z <= '3'; z++)
+		
 		_putchar(z);
-		_putchar(':');
 	}
+	_putchar(':');
 
 	for (a = '0'; a <= '5'; a++)
 	{
 		_putchar(a);
-	}
-	for (c = '0'; c <= '9'; c++)
-	{
+		for (c = '0'; c <= '9'; c++)
+
 		_putchar(c);
 	}
 	_putchar('\n');
