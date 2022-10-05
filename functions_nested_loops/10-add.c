a00dfcb0262831e54fd28a8f4f9c21ec3230b79b
AngelJCT <acarrion_pr@yahoo.com>
2022-10-05 09:28:35 -0700

    Add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * add - print the add of two integers
+ * @a: print the add of two integers
+ * @b: print the add of two ntegers
+ * Return: the add of two integers
+ *
+ */
+int add(int a, int b)
+{
+	int a;
+	int b;
+
+	a = 8;
+	b = 5;
+	add(8, 5);
+
+	_putchar("%d", a, b);
+	return (0);
+}
