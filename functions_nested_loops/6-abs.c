a80a4c7832ed8e06781203f396f6e163c940ef34
AngelJCT <acarrion_pr@yahoo.com>
2022-10-05 05:57:36 -0700

    Print signs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_abs - compute the absolute value of an integer
+ *@n: absolute value of an integer
+ *Return: Always 0 success
+ */
+int _abs(int n)
+{
+
+	int abNum = abs(n);
+
+	_putchar("%d", abNum);
+	_putchar('\n');
+
+	return (0);
+}
+
+
