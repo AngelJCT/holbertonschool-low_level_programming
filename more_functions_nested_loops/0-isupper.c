5d3224b358401acae7267ae367a3355636d5120e
AngelJCT <acarrion_pr@yahoo.com>
2022-10-06 11:32:50 -0700

    0main

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - return 1 if c is upper, 0 if not
+ * @c: return 1 if c is upper, 0 if not
+ * Return: Always 0 success
+ */
+int _isupper(int c)
+{
+	if (c > 'A' && c < 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
