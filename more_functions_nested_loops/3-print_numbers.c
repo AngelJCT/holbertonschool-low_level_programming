51e9846aba28d0d4584e0833472d43bcbd208d6a
AngelJCT <acarrion_pr@yahoo.com>
2022-10-06 15:52:27 -0700

    0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print 0 to 9
+ *
+ * Return: Always 0 success
+ */
+void print_numbers(void)
+{
+	int exe;
+
+	for (exe >= '0'; exe <= '9'; exe++)
+	{
+		_putchar(exe);
+	}
+	_putchar('\n');
+}
