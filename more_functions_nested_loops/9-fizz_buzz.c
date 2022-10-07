1fd80ac88a259b55c3ef4133b18db8950a5e3599
AngelJCT <acarrion_pr@yahoo.com>
2022-10-07 12:37:06 -0700

    FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio>
+/**
+ * main - print numbers 1 to 100
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int dig;
+
+	for (dig = 1; dig <= 100; dig++)
+	{
+		if (dig % 3 == 0 && dig % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (dig % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (dig % 5 == 0)
+		{
+			if (dig != 100)
+				printf("Buzz");
+			else
+				printf("Buzz");
+		}
+		else
+		{
+			printf("%i", dig);
+		}
+	}
+	printf("\n");
+	return (0);
+}
