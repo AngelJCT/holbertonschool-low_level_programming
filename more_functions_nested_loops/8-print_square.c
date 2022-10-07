3d1dc854c7922eef7babb62f23b35c06a24cb818
AngelJCT <acarrion_pr@yahoo.com>
2022-10-07 11:08:30 -0700

    Print square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -15,9 +15,9 @@ void print_square(int size)
 	}
 	else
 	{
-		for (a = 0; size > a; a++)
+		for (a = 0; a < size; a++)
 		{
-			for (b = 0; size > b; b++)
+			for (b = 0; b < size; b++)
 			{
 			_putchar(35);
 			}
