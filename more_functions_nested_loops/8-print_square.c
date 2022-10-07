df019ecf5c1db9e93eb39934135ae03c090cecae
AngelJCT <acarrion_pr@yahoo.com>
2022-10-07 10:50:08 -0700

    Print square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - print a square
+ * @size: print a square
+ * Return: Always 0 success
+ */
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; size > a; a++)
+		{
+			for (b = 0; size > b; b++)
+			{
+			_putchar(35);
+			}
+		}
+	}
+	_putchar('\n')
+}
