8dc48faa666f36096efaa482ba4250b5ce79f71c
AngelJCT <acarrion_pr@yahoo.com>
2022-10-07 05:12:21 -0700

    More numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers 0 to 14
+ *
+ * Return: Always 0 success
+ */
+void more_numbers(void)
+{
+	int A;
+	int J;
+
+	for (A = 0; A <= 9; A++)
+	{
+		for (J = 0; J <= 14; J++)
+		{
+			_putchar(J);
+		}
+		_putchar('\n');
+	}
+}
