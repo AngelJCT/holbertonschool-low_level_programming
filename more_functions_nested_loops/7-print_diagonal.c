bb3aa103d9998e41d7c80f08a3949d79b4736a86
AngelJCT <acarrion_pr@yahoo.com>
2022-10-07 08:50:56 -0700

    Print diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_diagonal - print a diagonal line
+ * @n: draw a diagonal line
+ * Return: Always 0 success
+ */
+void print_diagonal(int n)
+{
+	int x;
+
+	for (x = 0; n > x; x++)
+	{
+		_putchar('\');
+		_putchar('\n');
+	}
+}
