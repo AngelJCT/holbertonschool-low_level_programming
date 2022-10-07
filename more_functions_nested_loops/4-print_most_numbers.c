e74943ebd08219435b4ac9aa154d5a44a9e9929b
AngelJCT <acarrion_pr@yahoo.com>
2022-10-07 04:36:39 -0700

    Print numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers 0 to 9 except 2 and 4
+ * Return: Always 0 success
+ */
+void print_most_numbers(void)
+{
+	int t;
+
+	for (t = '0'; t <= '9'; t++)
+	{
+		if (t == '2' && t == '4)
+		{
+			return (0);
+		}
+		_putchar(t);
+	}
+	_putchar('\n');
+}
