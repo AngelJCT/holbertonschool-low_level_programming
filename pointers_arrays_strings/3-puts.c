450eae6085813081c0551b65b95bd026a180a50e
AngelJCT <acarrion_pr@yahoo.com>
2022-10-10 13:11:41 -0700

    Puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -8,10 +8,10 @@ void _puts(char *str)
 {
 	int a = 0;
 
-	while (*(str + a) != '\0')
+	while (*str != '\0')
 	{
-		_putchar(str[a]);
-		a++;
+		_putchar(a);
+		str++;
 	}
-	_puts(str);
+	_putchar('\n');
 }
