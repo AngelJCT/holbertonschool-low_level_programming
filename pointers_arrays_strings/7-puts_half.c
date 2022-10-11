dfd718c92d65fa64a7d752050edd191cf59c6843
AngelJCT <acarrion_pr@yahoo.com>
2022-10-11 08:09:05 -0700

    Puts half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *puts_half - print half of a string
+ *@str: print half of a string
+ *Return: Always 0 success
+ */
+void puts_half(char *str)
+{
+	int l, e, o;
+
+	l = 0;
+
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	if (l % 2 == 0)
+	{
+		for (e = l / 2; str[e] != '\0'; e++)
+		{
+			_putchar(str[e]);
+		}
+	}
+	else
+	{
+		for (o = (l - 1) / 2; o < l - 1; o++)
+		{
+			_putchar(o + 1);
+		}
+	}
+	_putchar('\n');
+}
