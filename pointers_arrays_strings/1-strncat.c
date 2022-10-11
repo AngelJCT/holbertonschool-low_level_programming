f67409630afc2727fd8e8ca90bcc1e600ce42d75
AngelJCT <acarrion_pr@yahoo.com>
2022-10-11 13:48:04 -0700

    Strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strncat - concatenate two string
+ *@dest: destination string
+ *@src: string
+ *@n: number of bytes
+ *Return: destination
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x, y;
+
+	x = 0;
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+
+	for (y = 0; src[y] && x < n; x++)
+	{
+		dest[x++] = src[y];
+	}
+	return (dest);
+}
