a0e671799ac0795aff5308bb8b8aba587a519932
AngelJCT <acarrion_pr@yahoo.com>
2022-10-11 09:17:07 -0700

    Print array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,13 +9,17 @@ void print_array(int *a, int n)
 {
 	int j;
 
-	for (j = 0; j < n; j++)
+	j = 0;
+	while (j < n)
 	{
-		printf("%d, ", *(a + j));
-
 		if (j == n)
 		{
 			printf("%d", *(a + j));
 		}
+		else
+		{
+			printf("%d, ", *(a + j));
+		}
+		j++;
 	}
 }
