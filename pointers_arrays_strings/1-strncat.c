872731476a482ee00df0185ed90c452ba51f6d98
AngelJCT <acarrion_pr@yahoo.com>
2022-10-11 15:44:41 -0700

    Strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,12 +11,18 @@ char *_strncat(char *dest, char *src, int n)
 	int x, y;
 
 	x = 0;
-	while (dest[x++] != '\0')
+	while (dest[x])
 	{
-		x++;
+		x++
 	}
-
-	for (y = 0; src[y] > n; y++)
+	y = 0;
+	while (y < n && src[y])
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	if (y < n)
 	{
 		dest[x] = src[y];
 	}
