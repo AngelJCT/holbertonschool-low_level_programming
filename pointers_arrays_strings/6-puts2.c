6c44cc37cdac3219d1fdb72c73660d34f1aeceb6
AngelJCT <acarrion_pr@yahoo.com>
2022-10-11 06:59:45 -0700

    Puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *puts2 - print characters of a string
+ *@str: return characters of a string startin from 0
+ *Return: Always 0 success
+ */
+void puts2(char *str)
+{
+	int x;
+
+	x = 0;
+	while (str[x] != '\0')
+	{
+		if (x % '2' == '0')
+		{
+			_putchar(x);
+		}
+		x++;
+	}
+	_putchar('\n');
+}
