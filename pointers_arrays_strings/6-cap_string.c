e2a89ae44a3b7bafec09ff6c252579425c109aed
AngelJCT <acarrion_pr@yahoo.com>
2022-10-12 07:50:48 -0700

    Capitalize

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,40 +7,23 @@
  */
 char *cap_string(char *xy)
 {
-	int u;
+	int u, t;
+	char c[13] = {'"', ' ', '.', '\n', '!', '-', '\t', ';', '(', ')', '{', '}','?',','};
 
 	for (u = 0; xy[u] != '\0'; u++)
 	{
-		if (u == 0)
+		if (u == 0 && xy[u] >= 'a' && xy[u] <= 'z')
 		{
-			if (xy[u]  >= 'a' && xy[u] <= 'z')
-			{
-				xy[u] = xy[u] - 32;
-			}
-			continue;
+			xy[u] -= 32;
 		}
-		if (xy[u] == '!')
+		for (n = 0; n < 13; n++)
 		{
-			++u;
-			if (xy[u] >= 'a' && xy[u] <= 'z')
+			if ( xy[u] == c[n])
 			{
-				xy[u] = xy[u] - 32;
-			}
-		}
-		if (xy[u] == ' ')
-		{
-			++u;
-			if (xy[u] >= 'a' && xy[u] <= 'z')
-			{
-				xy[u] = xy[u] - 32;
-			}
-		}
-		if (xy[u] == '.' &&  xy[u] == '\n')
-		{
-			++u;
-			if (xy[u] >= 'a' && xy[u] <= 'z')
-			{
-				xy[u] = xy[u] - 32;
+				if (xy[u + 1] >= 'a' && xy[u + 1] <= 'z')
+				{
+					xy[u + 1] -= 32;
+				}
 			}
 		}
 	}
