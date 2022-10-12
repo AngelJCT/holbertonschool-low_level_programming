fe9656ecc7d0690eca948cd6a5e4aaf4690a834a
AngelJCT <acarrion_pr@yahoo.com>
2022-10-12 04:42:10 -0700

    Capitalize

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ *cap_string - capitalize first letter of each word
+ *@xy: letters
+ *Return: each word capitalized
+ *
+ */
+char *cap_string(char *xy)
+{
+	int u;
+
+	for (u = 0; xy[u] != '\0'; u++)
+	{
+		if (u == 0)
+		{
+			if (xy[u]  >= 'a' && xy[u] <= 'z')
+			{
+				xy[u] = xy[u] - 32;
+			}
+		}
+		if (xy[u] == ' ')
+		{
+			++u;
+			if (xy[u] >= 'a' && xy[u] <= 'z')
+			{
+				xy[u] = xy[u] - 32;
+			}
+		}
+		else
+		{
+			if (xy[u] >= 'A' && xy[u] <= 'Z')
+			{
+				xy[u] = xy[u] + 32;
+			}
+		}
+	}
+	return (xy);
+}
