d2f6b20655e4e8afc521c9d0b842a5642597c1c8
AngelJCT <acarrion_pr@yahoo.com>
2022-10-11 17:22:55 -0700

    Strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,31 +3,15 @@
  *_strcmp - compare two strings
  *@s1: first string to be compared
  *@s2: second string to be compared
- *Return: Always 0 success
+ *Return: difference of s1 and s2
  */
 int _strcmp(char *s1, char *s2)
 {
 	int c = 0;
 
-	while (*s1 != '\0' || *s2 != '\0')
+	while (*s1[c] && *s2[c] && *s1[c] == *s2[c])
 	{
-		if (*s1 == *s2)
-		{
-			s1++;
-			s2++;
-		}
-		else if ((*s1 == '\0' && *s2 != '\0')
-			|| (*s1 != '\0' && *s2 == '\0')
-				|| *s1 != *s2)
-		{
-			c = 1;
-			return (1);
-			break;
-		}
+		c++;
 	}
-	if (c == 0)
-	{
-		return (0);
-	}
-	return (c);
+	return (*s1[c] - *s2[c]);
 }
