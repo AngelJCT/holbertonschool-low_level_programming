651dcfe537d8878d891e50b3b078df0d16003e94
AngelJCT <acarrion_pr@yahoo.com>
2022-10-12 18:58:18 +0000

    Leet

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+char *leet(char *ch)
+{
+	char lower[5] = {a, e, o, t, l};
+	char upper[5] = {A, E, O, T, L};
+	int num[5] = {4, 3, 0, 7, 1};
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		lower = ch[num];
+	}
+	return (ch)
+}
