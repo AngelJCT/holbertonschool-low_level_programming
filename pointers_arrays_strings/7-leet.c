b94765f36f3caeb48ed0005128850d8d5f2e066a
AngelJCT <acarrion_pr@yahoo.com>
2022-10-12 21:50:53 +0000

    Leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,13 +8,13 @@ char *leet(char *ch)
 {
 	int a, b;
 	char let[] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
-	char num[] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
+	char num[] = {4, 3, 0, 7, 1, 4, 3, 0, 7, 1};
 
 	for (a = 0; ch[a] != '\0'; a++)
 	{
 		for (b = '0'; b < 10; b++)
 		{
-			if (ch[a] == let[b])
+			if (ch[a] == let[a])
 			{
 				ch[a] = num[b];
 			}
