24ea0eabd1d6861633f405aebca1dc129724416f
AngelJCT <acarrion_pr@yahoo.com>
2022-10-11 17:24:18 -0700

    Strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,9 +9,9 @@ int _strcmp(char *s1, char *s2)
 {
 	int c = 0;
 
-	while (*s1[c] && *s2[c] && *s1[c] == *s2[c])
+	while (s1[c] && s2[c] && s1[c] == s2[c])
 	{
 		c++;
 	}
-	return (*s1[c] - *s2[c]);
+	return (s1[c] - s2[c]);
 }
