16903bbd34c12f6e2625268399f091052b1c7de5
AngelJCT <acarrion_pr@yahoo.com>
2022-10-12 04:18:30 -0700

    Lowercase to upper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *string_toupper - change all lowercase to uppercase
+ *@abc: letters
+ *Return: Always 0 success
+ */
+char *string_toupper(char *abc)
+{
+	int j;
+
+	for (j = 0; abc[j] != '\0'; j++)
+	{
+		if (abc[j] >= 'a' && abc[j] <= 'z')
+		{
+			abc[j] = abc[j] - 32;
+		}
+	}
+}
