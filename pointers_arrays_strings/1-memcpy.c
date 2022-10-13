39aeea2d7d79511b6c938a498e452f668fabc70e
AngelJCT <acarrion_pr@yahoo.com>
2022-10-13 12:33:10 +0000

    Memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_memcpy - copy memory to destination
+ *@dest: destination
+ *@src: memory to copy
+ *@n: number of bytes
+ *Return: destination memory area
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int b;
+
+	while (b < n && src[b])
+	{
+		dest[b] = src[b];
+		b++;
+	}
+	return (dest);
+}
+
