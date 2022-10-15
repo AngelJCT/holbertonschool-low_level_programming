d23735c4a67c6b61f0b2d7d85cb6468f488f6f98
AngelJCT <acarrion_pr@yahoo.com>
2022-10-15 14:48:20 +0000

    Print diagsums

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_diagsums - print sum of two diagonals
+ *@a: two dimensional array
+ *@size: number of digits
+ *Return: sum of two diagonals
+ */
+void print_diagsums(int *a, int size)
+{
+	int e, mus1, mus2;
+
+	for (e = 0; e < size; e++)
+	{
+		mus1 += *(a + (e * size + e));
+		mus2 += *(a + (e * size + (size - e - 1)));
+	}
+	printf("%d, %d\n", mus1, mus2);
+}
