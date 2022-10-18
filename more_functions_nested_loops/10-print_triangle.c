f42c6666fc0cdcaefe6e59a20b0e73e88f751fc8
AngelJCT <acarrion_pr@yahoo.com>
2022-10-18 12:18:02 +0000

    Triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *print_triangle - print triangle
+ *@size: size of triangle
+ *
+ *
+ */
+void print_triangle(int size)
+{
+	int r, sp, s2, bc;
+
+	s2 = size;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+		for (r = 0; r < size; ++)
+		{
+			for (sp = 1; sp < s2; sp++)
+			{
+				_putchar(' ');
+			}
+			if (r < size)
+			{
+				s2--;
+			}
+			for (bc = 0; bc <= r; bc++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+}
