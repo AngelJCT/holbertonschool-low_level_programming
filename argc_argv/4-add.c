aab2d39d43d3fe80c23160b6e1238a17f7f98dd1
AngelJCT <acarrion_pr@yahoo.com>
2022-10-20 00:32:47 +0000

    Add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -6,14 +6,21 @@
  *@argv: array of arguments
  *Return: sum of positive numbers
  */
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int sum, c;
+	char error[] = "Error";
 
 	sum = 0;
 	for (c = 1; c < argc; c++)
 	{
 		sum += atoi(argv[c]);
+
+		if (argc != c)
+		{
+			printf("%s\n", error);
+			return (1);
+		}
 	}
 	printf("%d\n", sum);
 	return (0);
