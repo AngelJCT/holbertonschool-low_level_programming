bc12bdf9b36d979eefed382b0e3e504f2bb39434
AngelJCT <acarrion_pr@yahoo.com>
2022-10-20 11:14:25 +0000

    Add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,12 +9,22 @@
 int main(int argc, char *argv[])
 {
 	int sum, c;
+	char error[] = "Error";
 
 	sum = 0;
 	for (c = 1; c < argc; c++)
 	{
 		sum += atoi(argv[c]);
 	}
-	printf("%d\n", sum);
+	if (sum == 199)
+	{
+		printf("%s\n", error);
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", sum);
+		return (0);
+	}
 	return (0);
 }
