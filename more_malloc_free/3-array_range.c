c1ef94c4b9c87d790f9144d023ef8c0b69d5a06c
AngelJCT <acarrion_pr@yahoo.com>
2022-10-26 17:31:19 +0000

    Array_range

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *array_range - print array of integers
+ *@min: smaller number
+ *@max: bigger number
+ *Return: array of numbers from min to max
+ */
+int *array_range(int min, int max)
+{
+	int i, j, n;
+	int *p;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	n = min + max;
+	if (n == 0)
+	{
+		n = 1;
+	}
+
+	p = malloc((sizeof(int) * n) + 4);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = min, j = 0; j < n; i++, j++)
+	{
+		p[j] = i;
+	}
+	return (p);
+}
