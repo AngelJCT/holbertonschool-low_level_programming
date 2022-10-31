1286a1d5b352c389cb04ba11d4eccaf94a8ee4f3
AngelJCT <acarrion_pr@yahoo.com>
2022-10-31 10:41:24 +0000

    Struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - struct description
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner
+ */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
+#endif
