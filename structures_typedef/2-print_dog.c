7410dba2cd40755888544359cd4cdf7e198e963f
AngelJCT <acarrion_pr@yahoo.com>
2022-10-31 12:27:25 +0000

    Print struct

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@ void print_dog(struct dog *d)
 	if (d->name == NULL || d->owner == NULL)
 	{
 		printf("Name: (nil)\n");
-		printf("Age: (nil)\n");
+		printf("Age:\n");
 		printf("Owner: (nil)");
 	}
 	else if (d == NULL)
