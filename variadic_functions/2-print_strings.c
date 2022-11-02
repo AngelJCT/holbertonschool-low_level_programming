e08358470994de6a7394711cfb49b9881d423aaf
AngelJCT <acarrion_pr@yahoo.com>
2022-11-02 16:35:00 +0000

    Print strings

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -25,6 +25,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", separator);
 		}
+		if (i == '\0')
+		{
+			printf("(nil)");
+		}
 	}
 	printf("\n");
 	va_end(ptr);
