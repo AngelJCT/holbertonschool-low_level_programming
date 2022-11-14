97359881df46416b1d6d067447d7a8f843b8922a
AngelJCT <acarrion_pr@yahoo.com>
2022-11-14 13:27:06 +0000

    0-print_list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "list.h"
+#include <stdio.h>
+/**
+ *print_list-function to print a linked list
+ *@h: pointer to head node
+ *
+ */
+size_t print_list(const list_t *h)
+{
+	int nElements = 0;
+
+	while (h != NULL)
+	{
+		printf("[%d] ", h->len);
+		if (h->str == NULL)
+		{
+			printf("(nil)");
+			printf("\n");
+		}
+		else
+		{
+			printf("%s", h->str);
+			printf("\n");
+		}
+			h = h->next;
+			nElements += 1;
+	}
+
+	return (nElements);
+}
