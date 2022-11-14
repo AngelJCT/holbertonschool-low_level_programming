ee3c353a9d31d6ad2ed9648addf87ab2624adabc
AngelJCT <acarrion_pr@yahoo.com>
2022-11-14 14:10:26 +0000

    1-list_len

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *list_len-return number of elements
+ *@h: pointer to head node
+ *Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	int nElements = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nElements +=1;
+	}
+	return (nElements);
+}
