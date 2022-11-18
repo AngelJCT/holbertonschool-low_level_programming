bc130f489864b678e53c965376723b285eaa63d2
AngelJCT <acarrion_pr@yahoo.com>
2022-11-18 16:01:39 +0000

    append text to file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -22,14 +22,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		len = strlen(text_content);
 		filewrite = write(fileopen, text_content, len);
 	}
-	else
-	{
-		return (-1);
-	}
 
 	if (fileopen == -1 || filewrite == -1)
 	{
-		return(-1);
+		return (-1);
 	}
 
 	close(fileopen);
