3de15a43777739818e6c3ee4d677f1206129833b
AngelJCT <acarrion_pr@yahoo.com>
2022-11-18 15:15:26 +0000

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fileopen, filewrite, len;
+	int fileopen, filewrite, len = strlen(text_content);
 
 	if (filename == NULL)
 	{
@@ -16,14 +16,13 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		len = strlen(text_content);
+		fileopen = open(filename, O_CREAT);
 	}
 	else
 	{
-		return (0);
+		fileopen = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		filewrite = write(fileopen, text_content, len);
 	}
-	fileopen = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	filewrite = write(fileopen, text_content, len);
 
 	if (fileopen == -1 || filewrite == -1)
 	{
