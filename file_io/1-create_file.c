415ec25699dbd93424bb6a2e52bfa375bd1e0c3d
AngelJCT <acarrion_pr@yahoo.com>
2022-11-18 14:09:37 +0000

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <string.h>
 /**
- *
- *
- *
- *
+ *create_file-function to create a file with text
+ *@filename: name of the file to be created
+ *@text_content: string to be written
+ *Return: file created with string
  */
 int create_file(const char *filename, char *text_content)
 {
