af51a35547ef8019e47a57c6cc60e16aa00bb4b7
AngelJCT <acarrion_pr@yahoo.com>
2022-11-18 13:44:28 +0000

    create file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <string.h>
+/**
+ *
+ *
+ *
+ *
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fileopen, filewrite;
+	size_t len = strlen(text_content);
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	text_content = malloc(sizeof(char));
+	if (text_content == NULL)
+	{
+		return (0);
+	}
+
+	fileopen = open(filename, O_CREAT | O_WRONLY);
+	filewrite = write(fileopen, text_content, len);
+
+	if (fileopen == -1 || filewrite == -1)
+	{
+		free(text_content);
+		return (-1);
+	}
+
+	free(text_content);
+	close(fileopen);
+	return (1);
+}
