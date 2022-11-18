ca67fb7d5a90fc937ac46ebd0f813505eaeb4cfc
AngelJCT <acarrion_pr@yahoo.com>
2022-11-18 15:13:07 +0000

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,14 +14,13 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	
 	if (text_content != NULL)
 	{
 		len = strlen(text_content);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 	fileopen = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	filewrite = write(fileopen, text_content, len);
