c04f3d5c70217f7ec559fde21e18865b29da3433
Parallels <acarrion_pr@yahoo.com>
2022-12-18 11:03:28 -0400

    printing comb of two digits from smallest comb

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main-entry point
+ *Return: Always 0 success
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (j != '9' || i != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
