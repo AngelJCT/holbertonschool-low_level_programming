16f719de5c5e94d1e558f25f28835dfcb245b8e4
Parallels <acarrion_pr@yahoo.com>
2023-01-02 09:54:39 -0400

    0-linear search, header, big O notation

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+/**
+ *linear_search-function to linear search an array of integers
+ *@array: pointer to first element of the array
+ *@size: number of elements in the array
+ *@value: value to search for
+ *Return: the index of value or -1 if its not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			return (i);
+			printf("%d\n", array[i]);
+		}
+	}
+	return (-1);
+}
